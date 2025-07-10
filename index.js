12408737563df877f9e1fbca7839cada7ccd7d5e
CameronJ0101 <cameronj010@gmail.com>
2025-07-10 18:03:42 -0500

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1 = 7 + 3;
+
+let code2 = 100 / 2 - 10;
+
+let code3 = 39;
+
+let message = "The vault has been secured. The combination is ";
+
+let codeA = "10-40-39" = code1 + "-" + code2 + "-" + code3;
+
+let codeB = "10-40-39" = code1 + "-" + code2 + "-" + code3;
+
+console.log(message + codeA + codeB);
+
